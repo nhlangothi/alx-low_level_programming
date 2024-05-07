206304c62b52a7cbff24d2fdbe51b026fdcc1b15
nhlangothi <mtambosiyabonga5@gmail.com>
2024-05-07 11:53:34 -0700

    betty

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,9 +1,16 @@
-#include "main.h"
+#include "main.hi"
+/**
+ * the name of function is _puts_recursion 
+ * and only has one parameter
+ */
 
-void _puts_recursion(char *s){
-    if( *s != '\0'){
+void _puts_recursion(char *s)
+{
+if( *s != '\0')
+{
     _putchar(*s);
     _puts_recursion(s+1);
-} else
+}
+     else
     _putchar('\n');
 }
