e6fe1aae5587983c1e8093290e601ac4dbc95d21
nhlangothi <mtambosiyabonga5@gmail.com>
2024-05-08 05:58:35 -0700

    2025

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,10 +1,8 @@
 #include "main.h"
 
-void _print_rev_recursion(char *s){
-    
-    if( *s != '\0'){
-        _print_rev_recursion(s +1);
-        _putchar(*s);
-        
-    }else _putchar('\n');
+void _print_rev_recursion(char *s) {
+	    if (*s != '\0') {
+		            _print_rev_recursion(s + 1);
+			            _putchar(*s);
+				        }
 }
